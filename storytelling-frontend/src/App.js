df6e3d7f2297881892de6db0103f0cdfb8607cc9
Md. Samiul Islam <95273981+MDSAMIULSAMI@users.noreply.github.com>
2024-09-05 15:58:16 +0600

    Add files via upload

---
+++ storytelling-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Story from './components/Story';
+import { CssBaseline } from '@mui/material';
+
+function App() {
+  return (
+    <>
+      <CssBaseline/>
+      <Story />
+    </>
+  );
+}
+
+export default App;
